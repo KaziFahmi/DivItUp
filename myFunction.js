70685ec85c6d1a5c869ffca08c2fa0ac47c5b756
Haque360 <shahilyasarhaque@gmail.com>
2022-08-30 23:21:26 +0600

    ProjectInfoPage Updated

--- myFunction.js
+++ myFunction.js
@@ -47,15 +47,18 @@ var t = 0;
 
 
   function openSubTab(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i =0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+    var i, tabcontent2, tablinks2;
+    tabcontent2 = document.getElementsByClassName("tabcontent2");
+    for (i =0; i < tabcontent2.length; i++) {
+      tabcontent2[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks2 = document.getElementsByClassName("tablinks2");
+    for (i = 0; i < tablinks2.length; i++) {
+      tablinks2[i].className = tablinks2[i].className.replace(" active", "");
     }
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
+
   }
+  document.getElementById("defaultOpen").click();
+  document.getElementById("defaultOpen2").click();
