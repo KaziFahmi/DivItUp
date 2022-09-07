fd8d6b223a98359f17da5fb8e53a85a67ab0fe90
KaziFahmi <60930748+KaziFahmi@users.noreply.github.com>
2022-09-07 16:06:17 +0600

    Update imageClass.php
    fixed merge bs

--- classes/imageClass.php
+++ classes/imageClass.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     class Image{
         private $name;
@@ -24,31 +23,5 @@
             return $this->image;
         }
     }
-=======
-<?php
-    class Image{
-        private $name;
-        private $fileType;
-        private $image;
 
-        function __construct($name, $fileType, $image){
-            $this->name = $name;
-            $this->fileType = $fileType;
-            $this->image = $image;
-        }
-
-        function getName(){
-            return $this->name;
-        }
-        function setName($name){
-            $this->name = $name;
-        }
-        function getFileType(){
-            return $this->fileType;
-        }
-        function getImage(){
-            return $this->image;
-        }
-    }
->>>>>>> 485a092b475cd73afeaff0f24abe7c2c63e32657
 ?>
