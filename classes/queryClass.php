ded08319bf4122b9b85e8096243144be3befeb38
KaziFahmi <60930748+KaziFahmi@users.noreply.github.com>
2022-09-07 16:09:49 +0600

    Update queryClass.php
    fixed merge bs

--- classes/queryClass.php
+++ classes/queryClass.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     // include 'budgetClass.php';
     // include 'commentsClass.php';
@@ -12,63 +11,97 @@
         public static function insertBudget($eventId, $budget, $connect){
             $insertQuery = $connect->prepare("INSERT INTO user (item_name, units, estimated_unit_price, actual_unit_price, date_expended, date_issued, event_id)
             VALUES (?, ?, ?, ?, ?, ?,?)");
-            $insertQuery->bind_param($budget->getItemName(), $budget->getUnits(), $budget->getEstimatedUnitPrice(), $budget->getActualUnitPrice(), $budget->getDateExpended, $budget->getDateIssued(), $eventId);
+            $itemname=$budget->getItemName()
+            $units=$budget->getUnits()
+            $estimatedunitprice=$budget->getEstimatedUnitPrice()
+            $actualunitprice= $budget->getActualUnitPrice()
+            $dateexpended=$budget->getDateExpended
+            $dateissued=$budget->getDateIssued()
+
+
+
+            $insertQuery->bind_param("siddss",$itemname , $units, $estimatedunitprice, $actualunitprice ,$dateexpended ,$dateissued , $eventId);
             $insertQuery->execute();
         }
 
         public static function insertComment($comment, $connect){
             $insertQuery = $connect->prepare("INSERT INTO comments (content, task_id, user_id)
             VALUES (?, ?, ?)");
-            $insertQuery->bind_param($comment->getContent(), $comment->getTaskId(), $comment->getUserID());
+            $content=$comment->getContent()
+            $taskid= $comment->getTaskId()
+            $userid= $comment->getUserID()
+
+        
+            $insertQuery->bind_param("sii",$content,$taskid,$userid );
             $insertQuery->execute();
         }
 
         public static function insertDocument($document, $eventId, $connect){
             $insertQuery = $connect->prepare("INSERT INTO document (file_name, file_type, file, event_id)
             VALUES (?, ?, ?)");
-            $insertQuery->bind_param($document->getName(), $document->getFileType(), $document->getDocument(), $eventId);
+            $name=$document->getName()
+            $filetype=$document->getFileType()
+            $document=$document->getDocument()
+     
+
+            $insertQuery->bind_param("sss", $name, $filetype,  $document, $eventId);
             $insertQuery->execute();
         }
 
         public static function insertEvent($event, $connect){
             $insertQuery = $connect->prepare("INSERT INTO event (event_id, name, summary, creatorId)
             VALUES (?, ?, ?, ?)");
-            $insertQuery->bind_param($event->getId(), $event->getName(), $event->getSummary(), $event->getCreatorId());
+            $id=$event->getId()
+            $name=$event->getName()
+            $summary=$event->getSummary()
+            $creatorid=$event->getCreatorId()
+
+             
+            $insertQuery->bind_param("issi",$id, $name, $summary, $creatorid);
             $insertQuery->execute();
         }
 
         public static function insertEventBoards($board, $eventId, $connect){
             $insertQuery = $connect->prepare("INSERT INTO event_board_column (board_column_name, event_id)
             VALUES (?, ?)");
-            $insertQuery->bind_param($board, $eventId);
+            $insertQuery->bind_param("ss",$board, $eventId);
             $insertQuery->execute();
         }
 
         public static function insertEventDepartments($department, $eventId, $connect){
             $insertQuery = $connect->prepare("INSERT INTO event_departments (department_name, event_id)
             VALUES (?, ?)");
-            $insertQuery->bind_param($department, $eventId);
+            $insertQuery->bind_param("si",$department, $eventId);
             $insertQuery->execute();
         }
 
         public static function insertMessages($message, $userId, $eventId, $connect){
             $insertQuery = $connect->prepare("INSERT INTO messages (content, user_id, event_id)
             VALUES (?, ?, ?)");
-            $insertQuery->bind_param($message, $userId, $eventId);
+            $insertQuery->bind_param("sii",$message, $userId, $eventId);
             $insertQuery->execute();
         }
 
         public static function insertNotification($summary, $openLink, $userId, $connect){
             $insertQuery = $connect->prepare("INSERT INTO notification (summary, openLink, user_id)
             VALUES (?, ?, ?)");
-            $insertQuery->bind_param($summary, $openLink, $userId);
+            $insertQuery->bind_param("ssi",$summary, $openLink, $userId);
             $insertQuery->execute();
         }
 
         public static function insertTask($task, $connect){
             $insertQuery = $connect->prepare("INSERT INTO task (task_id, summary, starting_time, deadline, status, event_id, user_id)
             VALUES (?, ?, ?, ?, ?, ?, ?)");
-            $insertQuery->bind_param($task->getId(), $task->getSummary(), $task->getStartingTime, $task->getDeadline(), $task->getStatus(), $task->getEventId(), $task->getAssigneeId());
+
+            $id= $task->getId()
+            $summary= $task->getSummary()
+            $startingTime= $task->getStartingTime
+            $deadeline= $task->getDeadline()
+            $status=$task->getStatus()
+            $eventid=$task->getEventId()
+            $assigneeid=$task->getAssigneeId()
+
+            $insertQuery->bind_param("issssii", $id , $summary , $startingTime , $deadeline, $status, $eventid,  $assigneeid);
             $insertQuery->execute();
         }
         
@@ -87,14 +120,14 @@
         public static function insertSkill($skill, $userId, $connect){
             $insertQuery = $connect->prepare("INSERT INTO user_skills (skills, user_id)
             VALUES (?, ?)");
-            $insertQuery->bind_param($skill, $userId);
+            $insertQuery->bind_param("si",$skill, $userId);
             $insertQuery->execute();
         }
 
         public static function insertWorksIn($department, $userId, $eventId, $connect){
             $insertQuery = $connect->prepare("INSERT INTO works_in (department, user_id, event_id)
             VALUES (?, ?, ?)");
-            $insertQuery->bind_param($deparment, $userId, $eventId, $connect);
+            $insertQuery->bind_param("sii",$deparment, $userId, $eventId, $connect);
             $insertQuery->execute();
         }
 
@@ -102,7 +135,6 @@
 
 
     }
-=======
 <?php
 include 'dbconnect.php';
     class Query{
@@ -178,10 +210,5 @@ include 'dbconnect.php';
                             VALUES ($$deparment, $userId, $eventId);";
             mysqli_query($connect, $insertQuery);
         }
-
-
-
-
     }
->>>>>>> 485a092b475cd73afeaff0f24abe7c2c63e32657
 ?>
