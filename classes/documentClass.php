19970ea16620b619be092fbda6c75d05630f4c51
KaziFahmi <60930748+KaziFahmi@users.noreply.github.com>
2022-09-07 16:04:23 +0600

    Update documentClass.php
    fixed merge bs

--- classes/documentClass.php
+++ classes/documentClass.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     class Document{
         private $name;
@@ -24,31 +23,4 @@
             return $this->Document;
         }
     }
-=======
-<?php
-    class Document{
-        private $name;
-        private $fileType;
-        private $document;
-
-        function __construct($name, $fileType, $document){
-            $this->name = $name;
-            $this->fileType = $fileType;
-            $this->document = $document;
-        }
-
-        function getName(){
-            return $this->name;
-        }
-        function setName($name){
-            $this->name = $name;
-        }
-        function getFileType(){
-            return $this->fileType;
-        }
-        function getDocument(){
-            return $this->Document;
-        }
-    }
->>>>>>> 485a092b475cd73afeaff0f24abe7c2c63e32657
 ?>
